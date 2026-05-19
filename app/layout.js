feb203cee9501ae13724d463b79115b5b573177c
Xander-Avey <xanderalvey@gmail.com>
2026-05-19 15:36:29 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K4M4PGVEVD"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K4M4PGVEVD');
+        `}} />
+      </head>
       <body style={{margin:0,padding:0}}>{children}</body>
     </html>
   )
